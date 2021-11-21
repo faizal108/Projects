18f6c00454a0e073267ebc9b90a6eb514468667f
Faizal <faizal@ganpat-student.com>
2021-11-21 22:53:52 +0530

    Projects

---
+++ protofolio/Tic-Tac-Toe/java.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class java {
+    public static void main(String[] args) {
+        System.out.println("hello world!!" + (2+5+10) );
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        System.out.println("Value of x is : "+x);
+        System.out.println("Value of x is : "+x+5);
+    }
+}
